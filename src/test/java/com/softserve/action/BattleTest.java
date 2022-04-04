b1b123a6e3a2edd2b978b849f87f95a6c41ccde1
Dmitry <dmitrymishnev@gmail.com>
2022-04-04 20:52:09 +0300

    refactor Warrior and Knight

--- src/test/java/com/softserve/action/BattleTest.java
+++ src/test/java/com/softserve/action/BattleTest.java
@@ -1,36 +1,43 @@
 package com.softserve.action;
 
-import com.softserve.model.Knight;
-import com.softserve.model.Warrior;
+import com.softserve.Battle;
+import com.softserve.Knight;
+import com.softserve.Warrior;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class BattleTest {
+    Warrior warrior;
+    Knight knight;
+
+    @BeforeEach
+    public void init() {
+        warrior = new Warrior();
+        knight = new Knight();
+    }
 
     @Test
     @DisplayName("Warrior vs Knight")
     void fightOne() {
-        var carl = new Warrior();
-        var jim = new Knight();
-        Battle.fight(carl, jim);
+        var carl = warrior;
+        var jim = knight;
         Assertions.assertFalse(Battle.fight(carl, jim));
     }
 
     @Test
     @DisplayName("Knight vs Warrior")
     void fightTwo() {
-        var ramon = new Warrior();
-        var slevin = new Knight();
+        var ramon = warrior;
+        var slevin = knight;
         Assertions.assertTrue(Battle.fight(slevin, ramon));
     }
 
     @Test
     @DisplayName("Warrior vs Warrior")
     void fightThree() {
-        var bob = new Warrior();
+        var bob = warrior;
         var mars = new Warrior();
         Battle.fight(bob, mars);
         Assertions.assertTrue(bob.isAlive());
@@ -39,8 +46,8 @@ class BattleTest {
     @Test
     @DisplayName("Knight vs Warrior. Knight has been a win")
     void fightFour() {
-        var zeus = new Knight();
-        var godKiller = new Warrior();
+        var zeus = knight;
+        var godKiller = warrior;
         Battle.fight(zeus, godKiller);
         Assertions.assertTrue(zeus.isAlive());
     }
@@ -48,8 +55,8 @@ class BattleTest {
     @Test
     @DisplayName("Warrior vs Warrior.  Second warrior has been a lose")
     void fightFive() {
-        var husband = new Warrior();
-        var wife = new Warrior();
+        var husband = warrior;
+        var wife = warrior;
         Battle.fight(husband, wife);
         Assertions.assertFalse(wife.isAlive());
     }
@@ -57,18 +64,19 @@ class BattleTest {
     @Test
     @DisplayName("Warrior vs Knight.  Knight has been a win")
     void fightSix() {
-        var dragon = new Warrior();
-        var knight = new Knight();
+        var dragon = warrior;
+        var knight = this.knight;
         Battle.fight(dragon, knight);
         Assertions.assertTrue(knight.isAlive());
     }
+
     @Test
     @DisplayName("Warrior_1 vs Knight and Knight vs Warrior_2")
     void fightSeven() {
-        var unit_1  = new Warrior();
-        var unit_2  = new Knight();
-        var unit_3  = new Warrior();
-        Battle.fight(unit_1 , unit_2 );
-        Assertions.assertFalse(Battle.fight(unit_2,unit_3));
+        var unit_1 = warrior;
+        var unit_2 = knight;
+        var unit_3 = new Warrior();
+        Battle.fight(unit_1, unit_2);
+        Assertions.assertFalse(Battle.fight(unit_2, unit_3));
     }
 }
